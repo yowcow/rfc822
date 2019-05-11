f89c552272618c96cd42d01635224a42affcc965
yowcow <yowcow@cpan.org>
2019-05-11 19:55:47 +0900

    from/to with timezone offset

--- src/rfc822.erl
+++ src/rfc822.erl
@@ -1,41 +1,135 @@
 -module(rfc822).
 
 -export([
-    from_now/1,
     from_datetime/1,
-    from_system_time/1
+    from_datetime/2,
+    from_system_time/1,
+    from_system_time/2,
+    from_timestamp/1,
+    from_timestamp/2,
+    to_datetime/1,
+    to_system_time/1,
+    to_timestamp/1
 ]).
 
-day(1) -> "Mon";
-day(2) -> "Tue";
-day(3) -> "Wed";
-day(4) -> "Thu";
-day(5) -> "Fri";
-day(6) -> "Sat";
-day(7) -> "Sun".
+dow(1) -> "Mon";
+dow(2) -> "Tue";
+dow(3) -> "Wed";
+dow(4) -> "Thu";
+dow(5) -> "Fri";
+dow(6) -> "Sat";
+dow(7) -> "Sun".
 
-month(1)  -> "Jan";
-month(2)  -> "Feb";
-month(3)  -> "Mar";
-month(4)  -> "Apr";
-month(5)  -> "May";
-month(6)  -> "Jun";
-month(7)  -> "Jul";
-month(8)  -> "Aug";
-month(9)  -> "Sep";
-month(10) -> "Oct";
-month(11) -> "Nov";
-month(12) -> "Dec".
+moy(1)  -> "Jan";
+moy(2)  -> "Feb";
+moy(3)  -> "Mar";
+moy(4)  -> "Apr";
+moy(5)  -> "May";
+moy(6)  -> "Jun";
+moy(7)  -> "Jul";
+moy(8)  -> "Aug";
+moy(9)  -> "Sep";
+moy(10) -> "Oct";
+moy(11) -> "Nov";
+moy(12) -> "Dec".
 
-from_datetime({{Year, Mon, Day}, {H, M, S}}) ->
-    lists:flatten(io_lib:format("~s, ~2.10.0B ~s ~4.10.0B ~2.10.0B:~2.10.0B:~2.10.0B GMT", [
-        day(calendar:day_of_the_week(Year, Mon, Day)),
-        Day, month(Mon), Year,
-        H, M, S
+mon("Jan") -> 1;
+mon("Feb") -> 2;
+mon("Mar") -> 3;
+mon("Apr") -> 4;
+mon("May") -> 5;
+mon("Jun") -> 6;
+mon("Jul") -> 7;
+mon("Aug") -> 8;
+mon("Sep") -> 9;
+mon("Oct") -> 10;
+mon("Nov") -> 11;
+mon("Dec") -> 12;
+mon(_)     -> 0.
+
+-define(DEFAULT_ZONE, "+0000").
+
+format_rfc822({{Year, Mon, Day}, {H, M, S}}, Z) ->
+    lists:flatten(io_lib:format("~3s, ~2.10.0B ~3s ~4.10.0B ~2.10.0B:~2.10.0B:~2.10.0B ~s", [
+        dow(calendar:day_of_the_week(Year, Mon, Day)),
+        Day, moy(Mon), Year,
+        H, M, S, Z
     ])).
 
-from_now(Now) ->
-    from_datetime(calendar:now_to_universal_time(Now)).
+-spec from_datetime(calendar:datetime()) -> string().
+from_datetime(DateTime) ->
+    from_datetime(DateTime, ?DEFAULT_ZONE).
 
-from_system_time(Time) ->
-    from_datetime(calendar:system_time_to_universal_time(Time, second)).
+-spec from_datetime(calendar:datetime(), string()) -> string().
+from_datetime(DateTime, "GMT") ->
+    format_rfc822(DateTime, "GMT");
+from_datetime(DateTime, "+0000") ->
+    format_rfc822(DateTime, "+0000");
+from_datetime(DateTime, "-0000") ->
+    format_rfc822(DateTime, "+0000");
+from_datetime(DateTime0, Z) ->
+    Offset = get_offset(Z),
+    Seconds = calendar:datetime_to_gregorian_seconds(DateTime0),
+    DateTime = calendar:gregorian_seconds_to_datetime(Seconds - Offset),
+    format_rfc822(DateTime, Z).
+
+-spec from_timestamp(erlang:timestamp()) -> string().
+from_timestamp(Timestamp) ->
+    from_timestamp(Timestamp, ?DEFAULT_ZONE).
+
+-spec from_timestamp(erlang:timestamp(), string()) -> string().
+from_timestamp(Timestamp, Z) ->
+    from_datetime(calendar:now_to_universal_time(Timestamp), Z).
+
+-spec from_system_time(integer()) -> string().
+from_system_time(Seconds) ->
+    from_system_time(Seconds, ?DEFAULT_ZONE).
+
+-spec from_system_time(integer(), string()) -> string().
+from_system_time(Seconds, Z) ->
+    from_datetime(calendar:system_time_to_universal_time(Seconds, second), Z).
+
+parse_datetime({ok, [_DoW, Day, Mon, Year, H, M, S, "GMT"], _}) ->
+    {{{Year, mon(Mon), Day}, {H, M, S}}, 0};
+parse_datetime({ok, [_DoW, Day, Mon, Year, H, M, S, "+0000"], _}) ->
+    {{{Year, mon(Mon), Day}, {H, M, S}}, 0};
+parse_datetime({ok, [_DoW, Day, Mon, Year, H, M, S, "-0000"], _}) ->
+    {{{Year, mon(Mon), Day}, {H, M, S}}, 0};
+parse_datetime({ok, [_DoW, Day, Mon, Year, H, M, S, Z], _}) ->
+    {{{Year, mon(Mon), Day}, {H, M, S}}, get_offset(Z)}.
+
+get_offset(Z) ->
+    parse_offset(io_lib:fread("~1s~2d~2d", Z)).
+
+parse_offset({ok, ["+", Hrs, Min], _}) ->
+    -3600*Hrs - 60*Min;
+parse_offset({ok, ["-", Hrs, Min], _}) ->
+    3600*Hrs + 60*Min.
+
+parse_rfc822(Str) ->
+    parse_datetime(io_lib:fread("~3s, ~2d ~3s ~4d ~2d:~2d:~2d ~s", Str)).
+
+to_datetime(Str) ->
+    case parse_rfc822(Str) of
+        {DateTime, 0} -> DateTime;
+        {DateTime, Offset} ->
+            Seconds = calendar:datetime_to_gregorian_seconds(DateTime),
+            calendar:gregorian_seconds_to_datetime(Seconds + Offset)
+    end.
+
+gregorian_seconds_to_system_time(Seconds) ->
+    Seconds - 62167219200. % gregorian seconds at 1970/01/01 00:00:00
+
+-spec to_system_time(list()) -> integer().
+to_system_time(Str) ->
+    {DateTime, Offset} = parse_rfc822(Str),
+    Seconds = calendar:datetime_to_gregorian_seconds(DateTime),
+    gregorian_seconds_to_system_time(Seconds + Offset).
+
+-spec to_timestamp(list()) -> erlang:timestamp().
+to_timestamp(Str) ->
+    Seconds = to_system_time(Str) * 1000000,
+    MegaSecs = Seconds div 1000000000000,
+    Secs = Seconds div 1000000 - MegaSecs*1000000,
+    MicroSecs = Seconds rem 1000000,
+    {MegaSecs, Secs, MicroSecs}.
--- test/rfc822_tests.erl
+++ test/rfc822_tests.erl
@@ -2,12 +2,12 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-from_datetime_test_() ->
+from_datetime1_test_() ->
     Cases = [
         {
             "2019-05-02T00:00:00Z",
             {{2019,5,2},{0,0,0}},
-            "Thu, 02 May 2019 00:00:00 GMT"
+            "Thu, 02 May 2019 00:00:00 +0000"
         }
     ],
     F = fun({Name, Input, Expected}) ->
@@ -16,30 +16,207 @@ from_datetime_test_() ->
     end,
     lists:map(F, Cases).
 
-from_now_test_() ->
+from_datetime2_test_() ->
     Cases = [
         {
-            "2019-05-02T09:53:17Z",
-            {1556,790797,212780},
-            "Thu, 02 May 2019 09:53:17 GMT"
+            "2019-05-02 00:00:00 GMT",
+            {{{2019,5,2},{0,0,0}}, "GMT"},
+            "Thu, 02 May 2019 00:00:00 GMT"
+        },
+        {
+            "2019-05-02 00:00:00 +0000",
+            {{{2019,5,2},{0,0,0}}, "+0000"},
+            "Thu, 02 May 2019 00:00:00 +0000"
+        },
+        {
+            "2019-05-02 00:00:00 -0000",
+            {{{2019,5,2},{0,0,0}}, "-0000"},
+            "Thu, 02 May 2019 00:00:00 +0000"
+        },
+        {
+            "2019-05-02 09:30:00 +0930",
+            {{{2019,5,2},{0,0,0}}, "+0930"},
+            "Thu, 02 May 2019 09:30:00 +0930"
+        },
+        {
+            "2019-05-01 20:30:00 -0330",
+            {{{2019,5,2},{0,0,0}}, "-0330"},
+            "Wed, 01 May 2019 20:30:00 -0330"
         }
     ],
-    F = fun({Name, Input, Expected}) ->
-        Actual = rfc822:from_now(Input),
+    F = fun({Name, {Input, Z}, Expected}) ->
+        Actual = rfc822:from_datetime(Input, Z),
         {Name, ?_assertEqual(Expected, Actual)}
     end,
     lists:map(F, Cases).
 
-from_system_time_test_() ->
+from_system_time1_test_() ->
     Cases = [
         {
             "2019-05-02T07:47:31Z",
             1556783251,
-            "Thu, 02 May 2019 07:47:31 GMT"
+            "Thu, 02 May 2019 07:47:31 +0000"
         }
     ],
     F = fun({Name, Input, Expected}) ->
         Actual = rfc822:from_system_time(Input),
         {Name, ?_assertEqual(Expected, Actual)}
+    end,
+    lists:map(F, Cases).
+
+from_system_time2_test_() ->
+    Cases = [
+        {
+            "2019-05-02 07:47:31 GMT",
+            {1556783251, "GMT"},
+            "Thu, 02 May 2019 07:47:31 GMT"
+        },
+        {
+            "2019-05-02 07:47:31 +0000",
+            {1556783251, "+0000"},
+            "Thu, 02 May 2019 07:47:31 +0000"
+        },
+        {
+            "2019-05-02 07:47:31 -0000",
+            {1556783251, "-0000"},
+            "Thu, 02 May 2019 07:47:31 +0000"
+        },
+        {
+            "2019-05-02 17:17:31 +0930",
+            {1556783251, "+0930"},
+            "Thu, 02 May 2019 17:17:31 +0930"
+        },
+        {
+            "2019-05-02 04:17:31 -0330",
+            {1556783251, "-0330"},
+            "Thu, 02 May 2019 04:17:31 -0330"
+        }
+    ],
+    F = fun({Name, {Input, Z}, Expected}) ->
+        Actual = rfc822:from_system_time(Input, Z),
+        {Name, ?_assertEqual(Expected, Actual)}
+    end,
+    lists:map(F, Cases).
+
+from_timestamp1_test_() ->
+    Cases = [
+        {
+            "2019-05-02T09:53:17Z",
+            {1556,790797,212780},
+            "Thu, 02 May 2019 09:53:17 +0000"
+        }
+    ],
+    F = fun({Name, Input, Expected}) ->
+        Actual = rfc822:from_timestamp(Input),
+        {Name, ?_assertEqual(Expected, Actual)}
+    end,
+    lists:map(F, Cases).
+
+from_timestamp2_test_() ->
+    Cases = [
+        {
+            "2019-05-02 09:53:17 GMT",
+            {{1556,790797,212780}, "GMT"},
+            "Thu, 02 May 2019 09:53:17 GMT"
+        },
+        {
+            "2019-05-02 09:53:17 +0000",
+            {{1556,790797,212780}, "+0000"},
+            "Thu, 02 May 2019 09:53:17 +0000"
+        },
+        {
+            "2019-05-02 09:53:17 -0000",
+            {{1556,790797,212780}, "-0000"},
+            "Thu, 02 May 2019 09:53:17 +0000"
+        },
+        {
+            "2019-05-02 18:23:17 +0930",
+            {{1556,790797,212780}, "+0930"},
+            "Thu, 02 May 2019 19:23:17 +0930"
+        },
+        {
+            "2019-05-02 06:23:17 -0330",
+            {{1556,790797,212780}, "-0330"},
+            "Thu, 02 May 2019 06:23:17 -0330"
+        }
+    ],
+    F = fun({Name, {Input, Z}, Expected}) ->
+        Actual = rfc822:from_timestamp(Input, Z),
+        {Name, ?_assertEqual(Expected, Actual)}
+    end,
+    lists:map(F, Cases).
+
+to_datetime_test_() ->
+    Cases = [
+        {
+            "Thu, 02 May 2019 00:00:00 GMT",
+            {{2019,5,2},{0,0,0}}
+        },
+        {
+            "Thu, 02 May 2019 00:00:00 +0000",
+            {{2019,5,2},{0,0,0}}
+        },
+        {
+            "Thu, 02 May 2019 09:30:00 +0930",
+            {{2019,5,2},{0,0,0}}
+        },
+        {
+            "Wed, 01 May 2019 20:30:00 -0330",
+            {{2019,5,2},{0,0,0}}
+        }
+    ],
+    F = fun({Input, Expected}) ->
+        Actual = rfc822:to_datetime(Input),
+        {Input, ?_assertEqual(Expected, Actual)}
+    end,
+    lists:map(F, Cases).
+
+to_system_test_() ->
+    Cases = [
+        {
+            "Thu, 02 May 2019 00:00:00 GMT",
+            1556755200
+        },
+        {
+            "Thu, 02 May 2019 00:00:00 +0000",
+            1556755200
+        },
+        {
+            "Thu, 02 May 2019 09:30:00 +0930",
+            1556755200
+        },
+        {
+            "Wed, 01 May 2019 20:30:00 -0330",
+            1556755200
+        }
+    ],
+    F = fun({Input, Expected}) ->
+        Actual = rfc822:to_system_time(Input),
+        {Input, ?_assertEqual(Expected, Actual)}
+    end,
+    lists:map(F, Cases).
+
+to_timestamp_test_() ->
+    Cases = [
+        {
+            "Thu, 02 May 2019 09:53:17 GMT",
+            {1556,790797,0}
+        },
+        {
+            "Thu, 02 May 2019 09:53:17 +0000",
+            {1556,790797,0}
+        },
+        {
+            "Thu, 02 May 2019 19:23:17 +0930",
+            {1556,790797,0}
+        },
+        {
+            "Thu, 02 May 2019 06:23:17 -0330",
+            {1556,790797,0}
+        }
+    ],
+    F = fun({Input, Expected}) ->
+        Actual = rfc822:to_timestamp(Input),
+        {Input, ?_assertEqual(Expected, Actual)}
     end,
     lists:map(F, Cases).
